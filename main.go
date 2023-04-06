5563c9d0d537e7cad762d716ff308b447e7a7da0
Sohel Mia <sohelcse1999@gmail.com>
2023-04-06 17:42:49 +0600

    delete bug fixed

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/cavaliergopher/grab/v3"
 	"github.com/inhies/go-bytesize"
 	"github.com/k0kubun/pp"
 	"github.com/mdsohelmia/gdl/pkg/downloader"
@@ -15,12 +16,12 @@ func main() {
 		Url: "https://player.vimeo.com/progressive_redirect/playback/807879296/rendition/1080p/file.mp4?loc=external&oauth2_token_id=1713368872&signature=8c17d337ee5ddee975c67bc74405206ea18045d30620d558f4caa3b2dfb3dded",
 	})
 
-	// downloader.Hook = func(response *grab.Response) {
-	// 	fmt.Printf("transferred %v / %v bytes (%.2f%%)\n",
-	// 		response.BytesComplete(),
-	// 		response.Size,
-	// 		100*response.Progress())
-	// }
+	downloader.Hook = func(response *grab.Response) {
+		fmt.Printf("transferred %v / %v bytes (%.2f%%)\n",
+			response.BytesComplete(),
+			response.Size,
+			100*response.Progress())
+	}
 
 	outputPath, err := downloader.Download(fmt.Sprintf("videos/%d.mp4", time.Now().Unix()))
 
